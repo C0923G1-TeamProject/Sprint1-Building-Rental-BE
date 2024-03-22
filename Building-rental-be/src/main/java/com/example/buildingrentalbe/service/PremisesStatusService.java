7619228386296ccdb292cbb4fb98c7a2ab721d0c
Trần Linh Giang <gt07839@gmail.com>
2024-03-22 14:03:19 +0700

    up lan dau

---
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/service/PremisesStatusService.java
@@ -0,0 +1,15 @@
+package com.example.buildingrentalbe.service;
+
+import com.example.buildingrentalbe.model.PremisesStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PremisesStatusService implements IPremisesStatusService {
+
+    @Override
+    public List<PremisesStatus> findAll() {
+        return null;
+    }
+}
