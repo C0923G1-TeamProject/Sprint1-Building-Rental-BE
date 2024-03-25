77aee48f90cb09579a06d3d45f9acf43477cc2b1
vanhieu272 <93814554+vanhieu272@users.noreply.github.com>
2024-03-25 11:28:50 +0700

    HIEU up premises
    11:28 25/03

--- Building-rental-be/src/main/java/com/example/buildingrentalbe/repository/IPremisesRepository.java
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/repository/IPremisesRepository.java
@@ -2,13 +2,19 @@ package com.example.buildingrentalbe.repository;
 
 import com.example.buildingrentalbe.model.Premises;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface IPremisesRepository extends JpaRepository<Premises, Integer> {
+public interface IPremisesRepository extends JpaRepository<Premises, Integer>, JpaSpecificationExecutor<Premises> {
     @Query(value = "select * from premises", nativeQuery = true)
     List<Premises> findAllPremises();
 
+    @Query(value = "select * from premises where id=:id", nativeQuery = true)
+    Premises findById(int id);
+
+    @Query(value = "select * from premises where code=:code", nativeQuery = true)
+    Premises findPremisesByCode(String code);
 
 }
