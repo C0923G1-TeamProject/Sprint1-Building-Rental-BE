5adc46f2fb117a3efac0ba9d96412d9dbc0b5448
vanhieu272 <93814554+vanhieu272@users.noreply.github.com>
2024-03-26 13:09:05 +0700

    HieuHTV
    Hieu update validate PremisesDTO + test case

--- Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/PremisesController.java
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/PremisesController.java
@@ -1,23 +1,32 @@
 package com.example.buildingrentalbe.controller;
 
+import com.example.buildingrentalbe.dto.PremisesDTO;
 import com.example.buildingrentalbe.model.Premises;
 import com.example.buildingrentalbe.service.impl.PremisesService;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping("/api")
+@RequestMapping("/api/premises")
 public class PremisesController {
 
     @Autowired
     PremisesService premisesService;
 
-    @GetMapping("/premises/search")
+    @GetMapping("/search")
     public ResponseEntity<Page<Premises>> searchPremises(
             @RequestParam( required = false) Integer floor,
             @RequestParam( required = false) String code,
@@ -25,7 +34,6 @@ public class PremisesController {
             @RequestParam( required = false) String premisesName,
             @RequestParam( defaultValue = "0") int page,
             @RequestParam( defaultValue = "10") int size) {
-
         Pageable pageable = PageRequest.of(page, size);
         Page<Premises> result = premisesService.searchPremises(floor, code, area, premisesName, pageable);
         if(result.getTotalPages() > 0){
@@ -33,5 +41,31 @@ public class PremisesController {
         }else {
             return ResponseEntity.noContent().build();
         }
+    }
+
+    @PatchMapping("update/{id}")
+    public ResponseEntity<?> updatePremises(@PathVariable("id") int id,
+                                            @Valid @RequestBody PremisesDTO premisesDTO,
+                                            BindingResult bindingResult) {
+
+        if (bindingResult.hasFieldErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getAllErrors());
+        }
+
+        Premises existingPremises = premisesService.findById(id);
+        if (existingPremises == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        if (!Objects.equals(existingPremises.getCode(), premisesDTO.getCode()) &&
+                premisesService.findPremisesByCode(premisesDTO.getCode()) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Mã mặt bằng đã tồn tại");
+        }
+
+        BeanUtils.copyProperties(premisesDTO, existingPremises, "id");
+
+        premisesService.updatePremises(id, existingPremises);
+
+        return ResponseEntity.ok("Cập nhật thông tin premises thành công");
     }
 }
