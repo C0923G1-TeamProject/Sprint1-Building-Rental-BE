ee1111b85dbc845c05b7f5b526d6266294dde397
viitran <143681243+viitran@users.noreply.github.com>
2024-03-27 10:04:05 +0700

    add file interface dto
    không git push file dto lên được mặc dù nhánh cá nhân có cho nên phải tạo file ntnay

---
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/dto/IInformationDto.java
@@ -0,0 +1,17 @@
+package com.example.buildingrentalbe.dto;
+
+public interface IInformationDto {
+    String getName();
+    String getCode();
+    String getDate();
+    Boolean isGender();
+    String getAddress();
+    String getPhoneNumber();
+    String getStartDate();
+    String getSalaryLevel();
+    String getDepartment();
+    String getEmail();
+    String getProfilePicture();
+    String getUsername();
+
+}
