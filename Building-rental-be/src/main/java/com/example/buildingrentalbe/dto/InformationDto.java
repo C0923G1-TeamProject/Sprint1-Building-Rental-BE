c30d8f250ddc49064cab382b53477023b43a45cb
viitran <tieuvi200904@gmail.com>
2024-03-30 21:45:45 +0700

    update BE information lan1

--- Building-rental-be/src/main/java/com/example/buildingrentalbe/dto/InformationDto.java
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/dto/InformationDto.java
@@ -7,6 +7,10 @@ import jakarta.validation.constraints.Size;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 
 public class InformationDto implements Validator {
     private Integer id;
@@ -14,12 +18,12 @@ public class InformationDto implements Validator {
     @NotBlank(message = "Ngày sinh không được để trống")
     private String date;
     @NotBlank(message = "Địa chỉ không được để trống")
-    @Pattern(regexp = "^[\\p{L}\\p{M}0-9\\s,.]+$",message = "Vui lòng nhập địa chỉ hợp lệ")
-    @Size(min = 4,max = 100,message = "Địa chỉ ít nhất phải chứa hơn 4 kí tự và không được chứa hơn 100 kí tự")
+    @Pattern(regexp = "^[\\p{L}\\p{M}0-9\\s,.]+$", message = "Vui lòng nhập địa chỉ hợp lệ")
+    @Size(min = 4, max = 100, message = "Địa chỉ ít nhất phải chứa hơn 4 kí tự và không được chứa hơn 100 kí tự")
     private String address;
     @NotBlank(message = "Họ và tên Không được để trống")
-    @Pattern(regexp = "^[\\p{L}\\p{M}\\s]+$",message = "Vui lòng nhập Họ và tên hợp lệ")
-    @Size(min = 1,max = 50,message = "Họ và tên ít nhất phải chứa hơn 1 kí tự và không được chứa hơn 50 kí tự")
+    @Pattern(regexp = "^[\\p{L}\\p{M}\\s]+$", message = "Vui lòng nhập Họ và tên hợp lệ")
+    @Size(min = 1, max = 50, message = "Họ và tên ít nhất phải chứa hơn 1 kí tự và không được chứa hơn 50 kí tự")
     private String name;
     private boolean gender;
     @NotBlank(message = "Email Không được để trống")
@@ -93,5 +97,18 @@ public class InformationDto implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
+            InformationDto informationDto = (InformationDto) target;
+            String pattern = "yyyy-MM-dd";
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+            try {
+                LocalDate birthday = LocalDate.parse(informationDto.getDate().toString(), formatter);
+                LocalDate now = LocalDate.now();
+                int age = Period.between(birthday, now).getYears();
+                if (age < 18){
+                    errors.rejectValue("date","","Bạn chưa đủ 18 tuổi!");
+                }
+            } catch (Exception e) {
+                errors.rejectValue("date", "", "age");
+            }
     }
 }
