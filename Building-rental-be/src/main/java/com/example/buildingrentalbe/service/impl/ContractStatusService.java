5e3ea49343cf3675fb2dfd6a1f91bfd73db5c1bd
viitran <tieuvi200904@gmail.com>
2024-03-30 23:56:43 +0700

    git up

---
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/service/impl/ContractStatusService.java
@@ -0,0 +1,27 @@
+package com.example.buildingrentalbe.service.impl;
+
+import com.example.buildingrentalbe.dto.IContractStatusDto;
+import com.example.buildingrentalbe.model.ContractStatus;
+import com.example.buildingrentalbe.repository.ThamRepository.IContractStatusRepository;
+import com.example.buildingrentalbe.service.ThamService.IContractStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContractStatusService implements IContractStatusService {
+    @Autowired
+    private IContractStatusRepository contractStatusRepository;
+    @Override
+    public List<ContractStatus> findAll() {
+        return contractStatusRepository.findAllStatus();
+    }
+
+    @Override
+    public Page<IContractStatusDto> getAll(Pageable pageable) {
+        return this.contractStatusRepository.getAll(pageable);
+    }
+}
