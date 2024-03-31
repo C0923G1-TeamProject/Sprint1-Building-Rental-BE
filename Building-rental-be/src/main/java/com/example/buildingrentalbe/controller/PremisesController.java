bce5dae39fa4095d0fb79ef6e93eee5729eb0f04
vanhieu272 <93814554+vanhieu272@users.noreply.github.com>
2024-03-31 15:25:07 +0700

    Update PremisesController.java

--- Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/PremisesController.java
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/PremisesController.java
@@ -47,7 +47,7 @@ public class PremisesController {
             @RequestParam( required = false) Float area,
             @RequestParam( required = false) String premisesName,
             @RequestParam( defaultValue = "0") int page,
-            @RequestParam( defaultValue = "5") int size) {
+            @RequestParam( defaultValue = "10") int size) {
         Pageable pageable = PageRequest.of(page, size);
         Page<Premises> result = premisesService.searchPremises(floor, code, area, premisesName, pageable);
         if(result.getTotalPages() > 0){
