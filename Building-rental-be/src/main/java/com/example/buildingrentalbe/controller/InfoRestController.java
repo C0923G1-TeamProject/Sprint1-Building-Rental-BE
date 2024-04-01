9365c77211e9af3b7903a3f44f1cf2e9181d01ac
viitran <tieuvi200904@gmail.com>
2024-04-01 09:10:39 +0700

    upd

--- Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/InfoRestController.java
+++ Building-rental-be/src/main/java/com/example/buildingrentalbe/controller/InfoRestController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -27,12 +28,14 @@ public class InfoRestController {
     @Autowired
     private IAccountService iAccountService;
 
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
     @GetMapping("/getInformationUser")
     public ResponseEntity<?> showInfoUser(Principal principal) {
         Account account = this.iAccountService.findAccountByUsername(principal.getName());
-        return new ResponseEntity<>(account,HttpStatus.OK);
+        return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
     @GetMapping("/account/{id}")
     public ResponseEntity<?> informationAccountUser(@PathVariable Integer id) {
         Account account = this.iAccountService.findAccountById(id);
@@ -42,11 +45,11 @@ public class InfoRestController {
         return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
-
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
     @PostMapping("/changed-password")
     public ResponseEntity<?> changePassword(@Valid @RequestBody PasswordDto passwordDto, BindingResult bindingResult, Principal principal) {
-        if (bindingResult.hasErrors()){
-            return new ResponseEntity<>(bindingResult.getAllErrors(),HttpStatus.BAD_REQUEST);
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
         }
         Account account = this.iAccountService.findAccountByUsername(principal.getName());
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
@@ -55,28 +58,33 @@ public class InfoRestController {
             if (passwordDto.getNewPassword().equals(passwordDto.getConfirmNewPassword())) {
                 this.iAccountService.changePassword(bCryptPasswordEncoder.encode(passwordDto.getNewPassword()), account.getId());
             } else {
-                return new ResponseEntity<>("Mật khẩu mới không trùng khớp với xác nhận mật khẩu!",HttpStatus.BAD_REQUEST);
+                System.out.println("Mk moi ko trung voi confirm mk");
+                return new ResponseEntity<>("Mật khẩu mới không trùng khớp với xác nhận mật khẩu!", HttpStatus.BAD_REQUEST);
             }
         } else {
-            return new ResponseEntity<>("Mật khẩu không chính xác!",HttpStatus.BAD_REQUEST);
+            System.out.println("sai mk");
+            return new ResponseEntity<>("Mật khẩu không chính xác!", HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>("Đổi mật khẩu thành công",HttpStatus.OK);
+        System.out.println("ok");
+        return new ResponseEntity<>("Đổi mật khẩu thành công", HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
     @PostMapping("/updateInformation")
-    public ResponseEntity<?> updateInformation(@Valid @RequestBody InformationDto informationDto, BindingResult bindingResult){
-        if (bindingResult.hasErrors()){
-            return new ResponseEntity<>(bindingResult.getAllErrors(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> updateInformation(@Valid @RequestBody InformationDto informationDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return new ResponseEntity<>(bindingResult.getAllErrors(), HttpStatus.BAD_REQUEST);
         }
         Employee employee = new Employee();
-        BeanUtils.copyProperties(informationDto,employee);
+        BeanUtils.copyProperties(informationDto, employee);
         this.iAccountService.updateInformationUser(employee);
-        return new ResponseEntity<>(employee,HttpStatus.OK);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
-@PostMapping("/updateImage")
-    public ResponseEntity<?> updateImage(@RequestBody ImageDto imageDto){
-    this.iAccountService.updateImage(imageDto);
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
+    @PostMapping("/updateImage")
+    public ResponseEntity<?> updateImage(@RequestBody ImageDto imageDto) {
+        this.iAccountService.updateImage(imageDto);
         return new ResponseEntity<>(HttpStatus.OK);
-}
+    }
 }
